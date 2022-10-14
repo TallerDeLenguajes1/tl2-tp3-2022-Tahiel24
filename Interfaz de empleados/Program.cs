1aceba628006dd354757f1f902e1e63466b034ce
Tahiel24 <tahielvarela@gmail.com>
2022-10-14 14:44:02 -0300

    Cambios finales

--- Interfaz de empleados/Program.cs
+++ Interfaz de empleados/Program.cs
@@ -8,7 +8,7 @@ namespace TP3{
             List<Pedido>ListadoPedidosN=new List<Pedido>();
             string[]lineas=File.ReadAllLines(@"C:\TALLER 2\REPOS\Interfaz de empleados\Cadetes.csv");
             List<string>contenido=new List<string>();
-            int cantPedidos,cont=1;
+            int cantPedidos;
             foreach(var i in lineas)
             {
                 string[]fila=i.Split(",");
@@ -24,14 +24,12 @@ namespace TP3{
             Console.WriteLine("2_Asignarlos a cadetes ");
             Console.WriteLine("3_Cambiar de estado el pedido");
             Console.WriteLine("4_Cambiar pedidos de cadetes");
-            Console.Write("Opcion: );
-            /*Console.WriteLine("5_ Dar por finalizado el dia");*/
+            Console.Write("Opcion: ");
             int opcion;
             do
             {
                 opcion=Convert.ToInt32(Console.ReadLine());
             }while(opcion<1&&opcion>4);
-            Console.WriteLine("\n");
             while (opcion != 5)
             {
                 switch (opcion)
@@ -45,36 +43,32 @@ namespace TP3{
                             Cliente nuevoCliente=new Cliente();
                             ListadoClientesN.Add(nuevoCliente);
                             Pedido nuevoPedido=new Pedido(nuevoCliente.Nombre,nuevoCliente.Direccion);
-                            ListadoPedidosN(nuevoPedido);
+                            ListadoPedidosN.Add(nuevoPedido);
             			}
                         Console.WriteLine("Pedidos subidos al sistema con exito");
                         break;
                     case 2:
                         Console.WriteLine("|||Asignar pedidos a cadetes|||");
-                        int numPedido,cade,op1,indice=0,indice2=0;
-                        if (ListadoPedidosN.Any())
-                        {
+                        int numPedido,cade,op1=0,indice=0,indice2=0,cont=1;
                             while (op1 != 2)
                             {
-                                if (ListadoPedidosN.Any()) {
                                 Console.WriteLine("Lista de pedidos y cadetes: ");
                                 foreach(var i in ListadoPedidosN)
                                 {
                                     Console.WriteLine("Cliente: "+i.Cliente+"-"+" Direccion: "+i.Dir+"- Numero de pedido: "+i.Nro);
                                 }
-                                Console.WriteLine("/n");
+                                Console.WriteLine("\n");
                                 foreach(var i in ListadoCadetes)
                                 {
                                     Console.WriteLine(cont+"_ "+"ID del cadete: "+i.Id);
                                     cont++;
                                 }
-                                Console.WriteLine("Seleccione el nro de pedido e id cadete a asignar:");
+                                Console.WriteLine("\nSeleccione el nro de pedido e id cadete a asignar:");
                                 Console.Write("Pedido: ");
                                 numPedido=Convert.ToInt32(Console.ReadLine());
-                                Console.WriteLine("/n");
-                                Console.WriteLine("Cadete: ");
+                                Console.Write("Cadete: ");
                                 cade=Convert.ToInt32(Console.ReadLine());
-                                Console.WriteLine("/n");
+                                Console.WriteLine("\n");
                                 foreach(var i in ListadoCadetes)
                                 {
                                     if (i.Id == cade)
@@ -95,36 +89,25 @@ namespace TP3{
                                     }
                                 }
                                 ListadoPedidosN.Remove(ListadoPedidosN[indice2]);
-                                Console.WriteLine("Desea agregar mas pedidos o salir?(1:salir, 2:seguir ");
+                                Console.Write("Desea agregar mas pedidos o salir?(2:Salir, 1:Seguir): ");
                                 do
                                 {
                                     op1=Convert.ToInt32(Console.ReadLine());
                                 }while(op1<1&&op1>2);
-
-                                }
-                                else
-                                {
-                                    Console.WriteLine("La lista de pedidos esta vacia, no puede asignarse a mas cadetes");
-                                    break;
-                                }
                             }
                             Console.WriteLine("Pedidos movidos con exito");
-                        }
-                        else
-                        {
-                            Console.WriteLine("La lista de pedidos esta vacia, ingrese pedidos al sistema primero(Opcion 1)");
-                            break;
-                        }
+                        break;
+                        
                 case 3:
-                        int op3,idC,idP,est,cont=0,cont1=0;
+                        int op3=0,idC,idP,est,cont3=0,cont1=0;
                         bool comp=false;
                         Console.WriteLine("Listado de pedidos y su estado: ");
                         for (int i = 0; i < ListadoCadetes.Count; i++)
 			            {
-                            foreach(var i in ListadoCadetes[i].ListadoPedidos)
+                            foreach(var ie in ListadoCadetes[i].ListadoPedidos)
                             {
                                 Console.WriteLine("Listado de pedidos y su estado(0:entregado,1:no entregado): ");
-                                Console.WriteLine("Id de cadete: " + ListadoCadetes[i].Id +"- Id de pedido: "+i.Nro+ "- Estado: "+i.Estado);
+                                Console.WriteLine("Id de cadete: " + ListadoCadetes[i].Id +"- Id de pedido: "+ie.Nro+ "- Estado: "+ie.Estado);
                             }
 			            }
 
@@ -132,10 +115,10 @@ namespace TP3{
                         {
                             for (int i = 0; i < ListadoCadetes.Count; i++)
 			                {
-                                foreach(var i in ListadoCadetes[i].ListadoPedidos)
+                                foreach(var ia in ListadoCadetes[i].ListadoPedidos)
                                 {
                                     Console.WriteLine("Listado de pedidos y su estado(0:entregado,1:no entregado): ");
-                                    Console.WriteLine("Id de cadete: " + ListadoCadetes[i].Id +"- Id de pedido: "+i.Nro+ "- Estado: "+i.Estado);
+                                    Console.WriteLine("Id de cadete: " + ListadoCadetes[i].Id +"- Id de pedido: "+ia.Nro+ "- Estado: "+ia.Estado);
                                 }
 			                }
                             Console.WriteLine("Ingrese id del cadete: ");
@@ -150,37 +133,28 @@ namespace TP3{
 			                {
                                 if (ListadoCadetes[i].Id == idC && comp==false)
                                 {
-                                    for (int j = 0; j < ListadoCadetes[i].ListadoPedidos.Count; j++)
-			                        {
-                                        if (comp == false)
-                                        {
-                                            foreach(var i in ListadoCadetes[i].ListadoPedidos[j])
+                                            foreach(var ig in ListadoCadetes[i].ListadoPedidos)
                                             {
-                                               if (i.Nro = idP)
+                                               if (ig.Nro == idP)
                                                {
-                                                    if (i == 0)
+                                                    if (est == 0)
                                                     {
-                                                        i.Estado=est;
-                                                        ListadoCadetes[i].ListadoEntregados.Add(i);
-                                                        ListadoCadetes[i].ListadoPedidos.Remove(i);
+                                                        ig.Estado=est;
+                                                        ListadoCadetes[i].ListadoEntregados.Add(ig);
+                                                        ListadoCadetes[i].ListadoPedidos.Remove(ig);
                                                         comp=true;
                                                         break;
                                                     }
-                                                    
                                                 }
                                                 else continue;
                                             }
-                                        }else break;
-                                        
-			                        }
-                                    
                                 }else continue;
 			                }
                             do
                             {
                                 Console.WriteLine("Desea cambiar mas pedidos(1) o salir(2)?");
                                 op3=Convert.ToInt32(Console.ReadLine());
-                            }while(op3<1 && op3>2)
+                            }while(op3<1 && op3>2);
                         }
                         Console.WriteLine("Pedidos modificados con exito");
                         break;
@@ -190,17 +164,18 @@ namespace TP3{
                 Console.WriteLine("3_Cambiar de estado el pedido");
                 Console.WriteLine("4_Cambiar pedidos de cadetes");
                 Console.WriteLine("5_Dar por finalizado el dia");
+                Console.Write("Opcion: ");
                 do
                 {
                     opcion=Convert.ToInt32(Console.ReadLine());
                 }while(opcion<1&&opcion>5);
             }
             int contFinal,valor;
-            Console.WriteLine("Dia finalizado, balance final: ");
+            Console.WriteLine("\nDia finalizado, balance final: ");
             foreach(var i in ListadoCadetes)
             {
                 valor=i.JornalACobrar();
-                Console.WriteLine("Nombre del cadete: "+i.Nombre+"- ID: "+i.Id+"- Cantidad de pedidos entregados: "+i.ListadoEntregados.Count+"- Jornal a cobrar: "+valor);
+                Console.WriteLine("Nombre del cadete: "+i.Nombre+" - ID: "+i.Id+" - Cantidad de pedidos entregados: "+i.ListadoEntregados.Count+" - Jornal a cobrar: "+valor);
             }
         }
     }
